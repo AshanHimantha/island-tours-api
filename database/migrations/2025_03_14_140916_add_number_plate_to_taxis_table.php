ae12f8a7917ecdaa7846e407b55f5c99cc696b5d
ashan321 <ashanhimantha321@gmail.com>
2025-03-14 19:40:09 +0530

    Add number plate field to taxi model and update validation rules

---
+++ database/migrations/2025_03_14_140916_add_number_plate_to_taxis_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('taxis', function (Blueprint $table) {
+            $table->string('number_plate', 20)->nullable()->unique()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('taxis', function (Blueprint $table) {
+            $table->dropColumn('number_plate');
+        });
+    }
+};
